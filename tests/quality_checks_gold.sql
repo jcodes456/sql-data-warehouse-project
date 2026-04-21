a07c5edb812a1f871e9b113ceabfd3ce1b543079
jcodes456 <jayotechwork@gmail.com>
2026-04-21 18:00:10 -0400

    Fix spelling errors in quality_checks_gold.sql
    Corrected spelling mistakes in comments and usage notes.

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -1,3 +1,20 @@
+*/
+-- ============================================================================
+Quality Checks
+-- ============================================================================
+Script Purpose:
+		This script perfromrs quality checks to validate the integrity, consistency,
+		and accuracy of the Gold layer. These checks ensure:
+		- Uniqueness of surrogate keys in dimension tables
+		- Referential integrity between fact and dimension tables
+		= Validation of relationships in the data model for analytical purposes
+
+Usage Notes:
+	- Run these checks after data loading Silver Layer
+	- Investigate and resolve any discrepencies found during the checks
+-- ============================================================================
+*/
+
 --Fixing descrepencies between the 2 sources for Gender
 SELECT DISTINCT
 	ci.cst_gndr,
